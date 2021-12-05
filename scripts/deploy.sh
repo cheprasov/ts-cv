f052b488f86d9571852e04deaadb3a4f8ab012a0
Alexander Cheprasov <acheprasov84@gmail.com>
2021-12-05 11:07:57 +0000

    cv upd

---
+++ scripts/deploy.sh
@@ -0,0 +1,2 @@
+
+rsync -avz --include="*" -e "ssh" ./../public/ linlruweb@77.222.61.31:/home/l/linlruweb/cheprasov.com/cv/public_html/
---
+++ scripts/update_hashes.ts
@@ -0,0 +1,64 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as process from 'process';
+
+const PUBLIC_DIR = path.resolve('./public');
+const INDEX_HTML = path.resolve(PUBLIC_DIR, 'index.html');
+
+const REPLACES: RegExp[] = [
+    /(?<=\b(src|href)=")[^"]+(?=")/g,
+];
+
+const html = fs.readFileSync(INDEX_HTML, { encoding: 'utf-8' });
+if (!html) {
+    console.error(`Can not read file ${INDEX_HTML}`);
+    process.exit(1);
+}
+
+const hashByFile = new Map<string, string>();
+const getHashForFile = (filename: string): string | null => {
+    let hash = hashByFile.get(filename);
+    if (hash) {
+        return hash;
+    }
+    const stats = fs.statSync(path.resolve(PUBLIC_DIR, filename)) as fs.Stats;
+    if (!stats) {
+        return null;
+    }
+    hash = stats.size.toString(32) + stats.mtime.getTime().toString(32);
+    hashByFile.set(filename, hash);
+    return hash;
+}
+
+const escapeRegExp = (value: string): string => {
+    return value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+const updatedHtml = REPLACES.reduce((html, regExp) => {
+    let newHtml = html;
+    // eslint-disable-next-line no-constant-condition
+    const matches = Array.from(newHtml.matchAll(regExp))
+    console.log('Found urls for updating', matches.map(m => m[0]));
+    matches.forEach((match) => {
+        const url = match[0];
+        if (!url) {
+            return;
+        }
+        const filename = url.split('?', 1)[0];
+        const hash = getHashForFile(filename);
+        let newUrl = url.replace(/[&?]hash=\w+\b/g, '');
+        newUrl = newUrl + (newUrl.includes('?') ? '&' : '?') + `hash=${hash}`;
+        if (newUrl === url) {
+            console.log('Do not need to update', url);
+        } else {
+            newHtml = newHtml.replace(RegExp(escapeRegExp(url), 'g'), newUrl);
+            console.log('Replaced urls: ', url, '=>', newUrl);
+        }
+    });
+
+    return newHtml;
+}, html);
+
+if (updatedHtml !== html) {
+    fs.writeFileSync(INDEX_HTML, updatedHtml, { encoding: 'utf-8' });
+}
