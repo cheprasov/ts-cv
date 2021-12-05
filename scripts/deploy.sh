95fb2e9f055f350b2dbc24ee0386d835b81943f2
Alexander Cheprasov <acheprasov84@gmail.com>
2021-12-05 12:36:22 +0000

    cv upd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,2 @@
 
-rsync -avzr --include="*" --exclude="*.DS_Store" -e "ssh" ./public/ linlruweb@77.222.61.31:/home/l/linlruweb/cheprasov.com/cv/public_html/
+rsync -avzr --exclude=.DS_Store -e "ssh" ./public/ linlruweb@77.222.61.31:/home/l/linlruweb/cheprasov.com/cv/public_html/
